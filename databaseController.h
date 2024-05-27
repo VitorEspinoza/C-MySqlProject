32646f1d9261c54f2a0ae09e3823648bcb3a6fbd
Vitor <vitor.espinozadg@gmail.com>
2024-05-27 12:03:08 -0300

    feat(TC-002): Adding update method in database controller

--- databaseController.h
+++ databaseController.h
@@ -24,6 +24,20 @@ Propriedade setPropriedade(char nome[50], char tipo[10], char valor[50]){
 	return P;
 }
 
+bool propertieIsNumber(Propriedade prop) {
+	const isNumber = prop.Tipo == "int" || prop.Tipo == "double";
+	return isNumber;
+}
+
+
+char* returnAttribution(Propriedade prop) {
+    if (propertieIsNumber(prop)) {
+        return "%s = %s";
+    } 
+    
+        return "%s = '%s'";
+    
+}
 MYSQL_RES *result;
 
 const int create(char tabela[50], int numPropriedades, ...) {
@@ -36,8 +50,6 @@ const int create(char tabela[50], int numPropriedades, ...) {
     int i;
     for (i = 0; i < numPropriedades; ++i) {
          propriedadesArray[i] = va_arg(args, Propriedade);
-        
-  
         sprintf(scriptSQL + strlen(scriptSQL), "%s", propriedadesArray[i].Nome);
 
         if (i < numPropriedades - 1) {
@@ -48,22 +60,7 @@ const int create(char tabela[50], int numPropriedades, ...) {
     sprintf(scriptSQL + strlen(scriptSQL), ") VALUES (");
 
     for (i = 0; i < numPropriedades; ++i) {
-
-        int resultado = strcmp(propriedadesArray[i].Tipo, "double");
-        
-        if (strcmp(propriedadesArray[i].Tipo, "int") == 0)
-		{
-            int valorInt = atoi(propriedadesArray[i].Valor);
-            sprintf(scriptSQL + strlen(scriptSQL), "%d", valorInt);
-        } else if (strcmp(propriedadesArray[i].Tipo, "double") == 0) 
-		{
-            double valorDouble = atof(propriedadesArray[i].Valor);
-            sprintf(scriptSQL + strlen(scriptSQL), "%.2f", valorDouble);
-        } else 
-		{
-            sprintf(scriptSQL + strlen(scriptSQL), "'%s'", propriedadesArray[i].Valor);
-        }
-
+       sprintf(scriptSQL + strlen(scriptSQL), "%s", propriedadesArray[i].Valor);
 		const isLastItem = i == numPropriedades - 1;
         if (!isLastItem) {
             sprintf(scriptSQL + strlen(scriptSQL), ", ");
@@ -74,25 +71,23 @@ const int create(char tabela[50], int numPropriedades, ...) {
 
     va_end(args);
     
-    printf(scriptSQL);
-
+  
   	int success = 0;
-			    if (mysql_ping(mySqlInstance)) {
-			        printf("ERROR: Impossível conectar.\n");
-			        printf("%s\n", mysql_error(mySqlInstance));
-			    }
-			
-			    if (mysql_query(mySqlInstance, scriptSQL)) {
-			        printf("ERROR: %s\n", mysql_error(mySqlInstance));
-			        rewind(stdin);
-			        getchar();
-			    } else {
-			        printf("Informações adicionadas corretamente.\n");
-			        success = 1;
-			    }
-  			
-				mysql_store_result(mySqlInstance);
-				
+    if (mysql_ping(mySqlInstance)) {
+        printf("ERROR: Impossível conectar.\n");
+        printf("%s\n", mysql_error(mySqlInstance));
+    }
+
+    if (mysql_query(mySqlInstance, scriptSQL)) {
+        printf("ERROR: %s\n", mysql_error(mySqlInstance));
+        rewind(stdin);
+        getchar();
+    } else {
+        printf("Informações adicionadas corretamente.\n");
+        success = 1;
+    }
+
+		                                                                         
     return success;
 }
 
@@ -164,6 +159,53 @@ MYSQL_ROW readByField(char tabela[50], Propriedade field)
 
 clearResult() {
 	mysql_free_result(result);
+}
+
+
+int update(char tabela[50], Propriedade identifierField, int numPropriedades, ...) {
+
+	char scriptSQL[500]; 
+    sprintf(scriptSQL, "UPDATE %s SET ", tabela);
+    va_list args;
+    va_start(args, numPropriedades);
+   
+	Propriedade propriedadesArray[numPropriedades];
+    int i;
+    for (i = 0; i < numPropriedades; ++i) {
+   
+        propriedadesArray[i] = va_arg(args, Propriedade);
+         
+	    char* format = returnAttribution(identifierField);
+		sprintf(scriptSQL + strlen(scriptSQL), format, propriedadesArray[i].Nome, propriedadesArray[i].Valor);
+		
+	
+        if (i < numPropriedades - 1) {
+            sprintf(scriptSQL + strlen(scriptSQL), ", ");
+        }
+    }
+    
+    char* format = returnAttribution(identifierField);
+	char whereClause[50];
+	sprintf(whereClause, " WHERE %s", format);
+    sprintf(scriptSQL + strlen(scriptSQL), whereClause, identifierField.Nome, identifierField.Valor);
+
+	int success = 0;
+	
+    if (mysql_ping(mySqlInstance)) {
+        printf("ERROR: Impossível conectar.\n");
+        printf("%s\n", mysql_error(mySqlInstance));
+    }
+
+    if (mysql_query(mySqlInstance, scriptSQL)) {
+        printf("ERROR: %s\n", mysql_error(mySqlInstance));
+        rewind(stdin);
+        getchar();
+    } else {
+        printf("Informações atualizadas corretamente.\n");
+        success = 1;
+    }
+    
+    return success;
 }
 
 
